2f3e5c5435708f8a91a8ef11cf9e74b040edd3b9
hywook4 <hywook4@naver.com>
2020-02-13 17:38:34 +0900

    directory modified, gitignore changed - config.json added

--- naver_api/naver_api.py
+++ naver_api/naver_api.py
@@ -3,7 +3,7 @@ import requests
 import urllib.request
 
 
-with open('/mnt/d/work_space/naver_hackaton_server/config.json') as config_file:
+with open('./config.json') as config_file:
     key = json.load(config_file)
 
 client_id = key['id']
@@ -12,6 +12,7 @@ client_secret = key['secret']
 
 
 ## api call for Clova Speech Recognize
+## CSR(audio_file) and it will return status code and recognized text
 def CSR(input_file):
     CSR_URL = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt"
     lang = "Kor"
@@ -39,6 +40,7 @@ def CSR(input_file):
 
 
 ## api call for Clova Speech Synthesis
+## CSS(text) : this will return mp3 binary file of given text
 def CSS(input_text):
     CSS_URL = "https://naveropenapi.apigw.ntruss.com/voice/v1/tts"
     encText = urllib.parse.quote(input_text)
@@ -61,8 +63,8 @@ def CSS(input_text):
 
 
 
-
 ## api call for Object Detection
+## OD(image) will give information of objects in image
 def OD(input_file):
     OD_URL = "https://naveropenapi.apigw.ntruss.com/vision-obj/v1/detect"
     files = {
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import make_response
 from flask import request
 from flask import jsonify
@@ -22,18 +22,21 @@ def index():
 ## Api for CSR
 @app.route('/CSR', methods=['POST'])
 def speech_recognition():
-    file = open('./test_files/csr_sample1.mp3', 'rb')
-    ret = naver_api.CSR(file)
+    value = request
+    #print(value)
+    #print(value.data)
+
+    #file = open('./test_files/csr_sample1.mp3', 'rb')
+    #ret = naver_api.CSR(file)
     
     
-    print(ret)
-    return ret
+    return "success"
 
 
 ## Api for CSS
 @app.route('/CSS')
 def speech_synthesis():
-    text = "안녕하세요 저는 미진이 입니다"
+    text = "자동자 1, 마우스 2"
     ret = naver_api.CSS(text)
     
     audio_file = ret["response_content"]
@@ -46,89 +49,48 @@ def speech_synthesis():
 
 
 ## Api for OD
-@app.route('/OD')
+@app.route('/OD', methods=["POST"])
 def object_detection():
-    file = open('./test_files/od_sample1.jpeg', 'rb')
+    value = request
+
+    file = value.data
     ret = naver_api.OD(file)
     
-
+    ## if response code is not 200, then return error
     if ret["response_code"] != 200:
         return "Error code : " + ret["response_code"]
 
     content = eval(ret["response_content"])
     content = content['predictions'][0]
-    print(content)
+    #print(content)
 
     object_info = {}
     object_info["names"] = []
     object_info["positions"] = []
 
+    # boundary for detection score
+    boundary = 0.75
+    
+    # use only objects that are recognized by higher score than boundary
     for a in range(len(content["detection_scores"])):
         probability = content["detection_scores"][a]
-        if probability >= 0.75:
+        if probability >= boundary:
             object_info["names"].append(eng_to_kor[content["detection_names"][a]])
             object_info["positions"].append(content["detection_boxes"][a])
     
 
-    ## 0:left, 1:middle, 2:right
-    pos_idx = []
-    for a in range(3):
-        pos_idx.append([])
+    # get string for client : object position representing string
+    ret_string = utils.get_object_positioned_string(object_info)
 
-    idx = 0
-    left = [0, 0, 1, 0.3333]
-    middle = [0, 0.3333, 1, 0.6666]
-    right = [0, 0.6666, 1, 1]
-    for l in object_info["positions"]:
-        max_area = 0
-        cur_area = 0
-        chosen_idx = 0
-        ## check left 
-        cur_area = utils.get_intersection_area(l, left)
-        if cur_area > max_area:
-            max_area = cur_area
-            chosen_idx = 0
 
-        ## check middle
-        cur_area = utils.get_intersection_area(l, middle)
-        if cur_area > max_area:
-            max_area = cur_area
-            chosen_idx = 1
-
-        ## check right
-        cur_area = utils.get_intersection_area(l, right)
-        if cur_area > max_area:
-            max_area = cur_area
-            chosen_idx = 2
-
-        if max_area <= 0.75:
-            pos_idx[chosen_idx].append(idx)
-
-        idx += 1
-
-        ret_string = ""
-
-        ret_string = "왼쪽에"
-        for a in pos_idx[0]:
-            ret_string +=  " " + object_info["names"][a]
-        ret_string += ". "
-
-        ret_string += "중간에"
-        for a in pos_idx[1]:
-            ret_string += " " + object_info["names"][a]
-        ret_string += ". "
-    
-        ret_string += "오른쪽에"
-        for a in pos_idx[2]:
-            ret_string += " " + object_info["names"][a]
-        ret_string += "가 있습니다."
-        
+    ## request text->audio conversion api
     ret = naver_api.CSS(ret_string)
     audio_file = ret["response_content"]
 
-    with open('./test_files/test_subeom.mp3', 'wb') as f: 
-        f.write(audio_file)
 
+    # save response audio file
+    #with open('./test_files/test.mp3', 'wb') as f: 
+    #    f.write(audio_file)
 
     return ret_string
 
@@ -139,7 +101,6 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    
     ## make dictionary for kor2eng, eng2kor
     with open('./mscoco.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
